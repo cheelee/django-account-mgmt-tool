8f7e3d42b920fdd8cf491f9ebec80286c2728e11
Chee Wai Lee <cheewai1972@gmail.com>
2019-05-16 18:17:07 +0800

    Reworked how I expect to deal with external downloaded resources, as well as stub/test data versus my own production data (which is to be kept private and local.) The current idea is to use softlinks to allow assets to be swapped in and out, without getting checked into the repository. Only the code needs to.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # Set up Python environment
-virtualenv env;
-source env/bin/activate
-pip install -r requirements.txt
+if [ ! -d env ] ; then
+    virtualenv env;
+    source env/bin/activate
+    pip install -r requirements.txt
+else
+    source env/bin/activate
+fi
 
 # Use the test database as the default demo database
+# The reason we make a copy instead of link is that usage of the database changes
+#   its timestamp, and this makes the repository mark it as a change.
 cp db.sqlite3.test db.sqlite3
 
 # Download and set up app static environment
 export appRoot=$PWD/account_tool
 mkdir -p externals;
 cd externals;
+
+echo "Installing Bootstrap 4 javascript and CSS files."
 if [ ! -d bootstrap-4.3.1-dist ] ; then
     if [ ! -f bootstrap-4.3.1-dist.zip ] ; then
 	echo "Downloading Bootstrap 4."
@@ -20,13 +28,17 @@ if [ ! -d bootstrap-4.3.1-dist ] ; then
     echo "Expanding Bootstrap 4 package."
     unzip bootstrap-4.3.1-dist.zip
 fi
-echo "Installing Bootstrap 4 javascript and CSS files."
-mkdir -p $appRoot/static/js
-cp bootstrap-4.3.1-dist/js/* $appRoot/static/js
-mkdir -p $appRoot/static/css
-cp bootstrap-4.3.1-dist/css/* $appRoot/static/css
+ln -sfn $PWD/bootstrap-4.3.1-dist $appRoot/static/bootstrap4
+echo "*****"
+
+echo "Installing jQuery 3.3.1."
 if [ ! -f jquery-3.3.1.min.js ] ; then
     wget https://code.jquery.com/jquery-3.3.1.min.js; 
 fi
-echo "Installing jQuery 3.3.1."
-cp jquery-3.3.1.min.js $appRoot/static/js
+mkdir -p $appRoot/static/js
+ln -sfn $PWD/jquery-3.3.1.min.js $appRoot/static/js/jquery.min.js
+echo "*****"
+
+echo "Installing Assets."
+ln -sfn $PWD/stubAssets/images $appRoot/static/images
+echo "*****"
